62e8619f07767aa54ac6a69447a4f50bb7044303
alex-antonov <antonovsasha22@mail.ru>
2019-08-17 19:53:50 +0700

    Рефактор хедера и меню. Добавлены svg картинки.

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,21 @@
-var navigation = document.querySelector('#navigation');
-var closeMenuButton = navigation.querySelector('#closeMenuModal');
-var openMenuButton = document.querySelector('#openMenuModal');
+var header = document.querySelector('#site-header');
+var toggleMenu = header.querySelector('#toggleMenu');
 
-navigation.classList.add('site-header__navigation--close');
 
-closeMenuButton.addEventListener('click', function () {
-  navigation.classList.add('site-header__navigation--close')
+header.classList.add('site-header--closed');
+toggleMenu.classList.add('site-header__toggle--open');
+
+toggleMenu.addEventListener('click', function () {
+
+  if (header.classList.contains('site-header--closed')) {
+    this.classList.add('site-header__toggle--close');
+    this.classList.remove('site-header__toggle--open');
+    header.classList.remove('site-header--closed');
+    header.classList.add('site-header--opened');
+    return;
+  }
+  this.classList.remove('site-header__toggle--close');
+  this.classList.add('site-header__toggle--open');
+  header.classList.add('site-header--closed')
 });
 
-openMenuButton.addEventListener('click', function () {
-  navigation.classList.remove('site-header__navigation--close')
-});
